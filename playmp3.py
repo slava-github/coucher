62c48c4d6a73463a36ee0e045adf588e6adf02ad
Vyacheslav Nechepurenko <vsn@yandex.ru>
2013-11-18 10:18:33 +0400

    * Начал работу над поигрыванием звуков в карточках

---
+++ playmp3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import subprocess as pr
+import httplib
+
+def play(fname):
+	pr.call('play '+fname)
+
+def playurl(url):
+
+
+if __name__ == '__main__':
+	playurl('http://d2x1jgnvxlnz25.cloudfront.net/v2/1/61567-631152008.mp3')
