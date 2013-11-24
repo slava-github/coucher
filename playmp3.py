6246af2ed5d23628dd048cf11b3c00fa40035ed4
Vyacheslav Nechepurenko <vsn@yandex.ru>
2013-11-24 15:51:11 +0400

    * Добавил класс проигрывание звуков (из файла или http)

--- playmp3.py
+++ playmp3.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 
 import subprocess as pr
-import httplib
+#import httplib
+import os
 
-def play(fname):
-	pr.call('play '+fname)
+CACHE_PATH = 'sound.cache'
+
+def playfile(fname):
+	pr.call(['play', fname])
 
 def playurl(url):
+	fname = "{}{}".format(CACHE_PATH, url[url.rfind('/'):])
+	if not os.path.exists(fname):
+		pr.call('curl {} >{}'.format(url, fname), shell=True)
+	playfile(fname)
 
+def play(name):
+	if name[0:4] == 'http':
+		playurl(name)
+	else:
+		playfile(name)
 
 if __name__ == '__main__':
-	playurl('http://d2x1jgnvxlnz25.cloudfront.net/v2/1/61567-631152008.mp3')
+	play('http://d2x1jgnvxlnz25.cloudfront.net/v2/1/61567-631152008.mp3')
