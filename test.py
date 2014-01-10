ba3b1142bf3411780ff20993574ec7968066b66c
Vyacheslav Nechepurenko <vsn@yandex.ru>
2014-01-10 22:09:32 +0400

    * тестовый словарь "таблица умножения"

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import coach
+import gui
+import sys
+
+def init(c):
+	for i in range(10):
+		for j in range(10):
+			c.add([coach.Task("{}x{}=".format(i, j), '', str(i*j), '')])
+
+if __name__ == '__main__':
+	
+	file_name = 'test.st'
+	c = coach.load(file_name)
+	if len(c) == 0:	init(c)
+	gui.start(c)
+	coach.save(file_name, c)
